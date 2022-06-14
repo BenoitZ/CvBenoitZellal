4ad01156f7f4da4e8efe2361711092578895374f
BenoitZ <103201206+BenoitZ@users.noreply.github.com>
2022-06-14 10:23:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let welcome = document.querySelector('h1');
+
+alert('Bienvenue sur mon CV !');
+
+
+               
+const myProfil = document.querySelector('h4');
+
+new Typewriter(myProfil)
+
+.changeDelay (60)
+.typeString ('Désireux de développer constamment mes compétences afin d’assurer mon évolution professionnelle, je suis confiant en ma capacité d’apprendre et avide de partage professionnel et créatif.')
+.start ()
